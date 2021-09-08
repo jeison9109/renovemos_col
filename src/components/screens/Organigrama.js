84b3b052c8cd821bb095b7a78beab00783b3dd07
Yeison Serna <yeisonivanserna@gmail.com>
2021-09-08 16:42:54 -0500

    add Screen organigrama

---
+++ src/components/screens/Organigrama.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Organigrama = () => {
+  return (
+    <>
+      <div className="separator">
+        <div className="line"></div>
+        <h2>Organigrama</h2>
+        <div className="line"></div>
+      </div>
+    </>
+  );
+};
